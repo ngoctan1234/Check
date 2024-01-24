1fea89777e54d830e5a2d9a3c5b5d46c579460ce
ngoctannew@gmail.com <v.d.tr.giang@gmail.com>
2024-01-24 20:47:33 +0700

    ud3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./Footer";
 
 function App() {
     return (
@@ -7,6 +8,7 @@ function App() {
             <h1>Tìm hiểu reactjs</h1>
             <p>Các components</p>
             <h2>UseState và useEffect</h2>
+            <Footer />
         </div>
     );
 }
---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <div>
+            <h1>Đây là footer</h1>
+        </div>
+    )
+}
