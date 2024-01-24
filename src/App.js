69e1c135894490f91eeec22c72f8d627ca15593e
ngoctan1234 <ngoctannew@gmail.com>
2024-01-24 20:50:16 +0700

    ud4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import Header from "./components/header";
 function App() {
     return (
         <div>
             <h1>Tìm hiểu reactjs</h1>
             <p>Các components</p>
             <h2>UseState và useEffect</h2>
+            <Header />
         </div>
     );
 }
